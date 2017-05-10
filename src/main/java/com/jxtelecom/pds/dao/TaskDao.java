e1e60e63b25a09ee3ac4090496a1c054098cb43e
tzj163 <200882514@qq.com>
2017-05-10 15:52:10 +0800

    欠费任务

---
+++ src/main/java/com/jxtelecom/pds/dao/TaskDao.java
@@ -0,0 +1,14 @@
+package com.jxtelecom.pds.dao;
+
+import com.jxtelecom.pds.entity.TaskEntity;
+
+/**
+ * 
+ * 
+ * @author jxtelecom
+ * @email jxtelecom@gmail.com
+ * @date 2017-05-10 11:25:19
+ */
+public interface TaskDao extends BaseDao<TaskEntity> {
+	
+}
